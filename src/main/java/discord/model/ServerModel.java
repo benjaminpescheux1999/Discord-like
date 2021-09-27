e684897de4c44561ef32300f9a05e51da57c6b80
ThomasN99 <thomasnevo99@gmail.com>
2021-09-27 15:52:39 +0200

    Create ServerModel.java

---
+++ src/main/java/discord/model/ServerModel.java
@@ -0,0 +1,37 @@
+package discord.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerModel {
+
+	private static int Id = 0;
+	private String Nom;
+	private List<String> ListUser=new ArrayList<String>();
+	private List<String> ListMessage=new ArrayList<String>();
+	
+// Constructeur
+	public ServerModel() {
+        Id++;
+    }
+
+    public ServerModel(String Nom, String Prenom, List<String> ListUser, List<String> ListMessage) {
+        Id++;
+        this.Nom = Nom;
+        this.ListUser = ListUser;
+        this.ListMessage = ListMessage;
+    }
+// Nom
+    public void setNom(String Nom) {
+        this.Nom=Nom;
+    }
+
+    public String getNom() {
+        return this.Nom;
+    }
+// Id 
+    public int getId() {
+        return this.Id;
+    }
+
+}
