cbc0bfd506c8f4572732a4dcf1dd18ba0e98c21b
benjaminpescheux1999 <47316003+benjaminpescheux1999@users.noreply.github.com>
2021-09-28 16:05:28 +0200

    JpaConfig

---
+++ src/main/java/discord/config/JpaConfig.java
@@ -0,0 +1,69 @@
+package discord.config;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement //Activer les annotations @Transactionnal
+@EnableJpaRepositories("discord.dao") // Activer spring-data-jpa
+public class JpaConfig {
+	
+	@Bean
+	public DataSource dataSource() {
+		
+		BasicDataSource dataSource = new BasicDataSource();
+		
+		dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		dataSource.setUrl("jdbc:sqlserver://localhost:1433;instance=SQLEXPRESS01;databaseName=eshop_iia");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("Not24Get");
+		dataSource.setMaxTotal(10);
+		
+		return dataSource;
+	}
+	
+	
+	//Configuration de EntityManagerFactory pour fabriquer des EntityManager
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		Properties hibernateProperties = new Properties();
+		
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+		hibernateProperties.setProperty("hibernate.show_sql", "true");
+		
+		emf.setDataSource(dataSource);
+		emf.setPackagesToScan("fr.formation.model");
+		emf.setJpaVendorAdapter(vendorAdapter);
+		emf.setJpaProperties(hibernateProperties);
+		
+		
+		
+		
+		return emf;
+	}
+	
+	//Configuration du gestionnaire de transactions
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		
+		transactionManager.setEntityManagerFactory(emf);
+		
+		return transactionManager;
+	}
+}
