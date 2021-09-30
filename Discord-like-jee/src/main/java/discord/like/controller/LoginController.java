0ba6068e90577271a4d9a93d4f7efd9f19939bb0
benjaminpescheux1999 <47316003+benjaminpescheux1999@users.noreply.github.com>
2021-09-30 16:05:28 +0200

    vos gueule

--- Discord-like-jee/src/main/java/discord/like/controller/LoginController.java
+++ Discord-like-jee/src/main/java/discord/like/controller/LoginController.java
@@ -1,5 +1,25 @@
 package discord.like.controller;
 
-public class LoginController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import discord.like.dto.LoginDto;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	@GetMapping
+	public String login() {
+		return "login";
+	}
+	@PostMapping
+	public String login(LoginDto login, Model model) {
+		model.addAttribute("utilisateur", login.getUsername());
+		model.addAttribute("password", login.getPassword());
+
+		return "home";
+	}
 }
