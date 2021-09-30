a9d9881cf6e559d62713526f250a58ec1ece479b
benjaminpescheux1999 <47316003+benjaminpescheux1999@users.noreply.github.com>
2021-09-30 16:39:54 +0200

    tiens

--- Discord-like-jee/src/main/java/discord/like/config/WebConfig.java
+++ Discord-like-jee/src/main/java/discord/like/config/WebConfig.java
@@ -1,5 +1,21 @@
 package discord.like.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+	@Bean
+	public UrlBasedViewResolver viewResolver() {
+		UrlBasedViewResolver viewResolver =new UrlBasedViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
 }
